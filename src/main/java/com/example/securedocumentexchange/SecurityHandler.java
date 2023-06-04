d818f4c8099caf419d75be005b431473c137f7e0
JoJo-as-JoJo <lekha.semenow2015@yandex.ru>
2023-06-04 10:51:25 +0300

    second com

--- src/main/java/com/example/securedocumentexchange/SecurityHandler.java
+++ src/main/java/com/example/securedocumentexchange/SecurityHandler.java
@@ -1,27 +1,16 @@
 package com.example.securedocumentexchange;
 
+import com.sshtools.common.publickey.SshKeyUtils;
+import com.sshtools.common.ssh.components.SshPublicKey;
+
 import javax.crypto.Cipher;
 import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.*;
 
 public interface SecurityHandler {
-//     private void encryptDocument(File document, File openKey) throws IOException, GeneralSecurityException {
-//        byte[] message;
-//        FileInputStream fis = new FileInputStream(document);
-//        message = fis.readAllBytes();
-//        fis.close();
-//        System.out.println(message);
-//        SshPublicKey sshPublicKey = SshKeyUtils.getPublicKey(openKey);
-//        publicKey = sshPublicKey.getJCEPublicKey();
-//        Key generatedAes = generateFromString(128);
-//        IvParameterSpec iv = generatedIv(generatedAes.getEncoded().length);
-//        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-//        cipher.init(Cipher.ENCRYPT_MODE, generatedAes, new GCMParameterSpec(128, iv.getIV()));
-//        byte[] encryptedBytes = cipher.doFinal(message);
-//        cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA256ANDMGF1PADDING");
-//     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.securedocumentexchange {
     requires javafx.controls;
     requires javafx.fxml;
+    requires maverick.synergy.client;
+    requires maverick.base;
 
 
     opens com.example.securedocumentexchange to javafx.fxml;
